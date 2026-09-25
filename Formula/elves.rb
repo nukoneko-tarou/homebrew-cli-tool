832beea5e1489d0c8b4e94bcc2e2e51727666fad
Ryo Beppu <122145746+nukoneko-tarou@users.noreply.github.com>
2026-09-25 23:36:58 +0900

    chore: update elves to v1.2.1

--- Formula/elves.rb
+++ Formula/elves.rb
@@ -1,25 +1,25 @@
 class Elves < Formula
   desc "Tool to generate project directory structures from JSON files compatible with tree -J"
   homepage "https://github.com/nukoneko-tarou/elves"
-  version "1.2.0"
+  version "1.2.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/nukoneko-tarou/elves/releases/download/v1.2.0/elves_1.2.0_darwin_arm64.tar.gz"
-      sha256 "bb9dc297df34cc8698ea465086c614e9cb0e6f559b11f465d0f6c047cb47922c"
+      url "https://github.com/nukoneko-tarou/elves/releases/download/v1.2.1/elves_1.2.1_darwin_arm64.tar.gz"
+      sha256 "b969b61d18d516edfd4039b444daa88e67191548cbcc4a12aeae4464aa08b0be"
     else
-      url "https://github.com/nukoneko-tarou/elves/releases/download/v1.2.0/elves_1.2.0_darwin_amd64.tar.gz"
-      sha256 "7c8dfdae37e3bf6b793cb48dc009021dd0645a65e2ae17b24f479590e8604d81"
+      url "https://github.com/nukoneko-tarou/elves/releases/download/v1.2.1/elves_1.2.1_darwin_amd64.tar.gz"
+      sha256 "669ed54e00a001a526ad3ee6017cc2d4b0e560dbbdda55bcbf2e848b80c509f1"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/nukoneko-tarou/elves/releases/download/v1.2.0/elves_1.2.0_linux_arm64.tar.gz"
-      sha256 "d322220adf126f1f1a4c68272acd144faa02cdb2758eb822f80036d587b01d8b"
+      url "https://github.com/nukoneko-tarou/elves/releases/download/v1.2.1/elves_1.2.1_linux_arm64.tar.gz"
+      sha256 "6ce71747d8342a48a6b8713653d6049ed10f245bb9a1887a095135a99e85b479"
     else
-      url "https://github.com/nukoneko-tarou/elves/releases/download/v1.2.0/elves_1.2.0_linux_amd64.tar.gz"
-      sha256 "31b7627f622510a242324ec648a76e63812bb0d9fbcffb7162030aff1c54c246"
+      url "https://github.com/nukoneko-tarou/elves/releases/download/v1.2.1/elves_1.2.1_linux_amd64.tar.gz"
+      sha256 "e37d1c02d55992253208f73e170d54cde29c336e815ddee0d13586705d1fd439"
     end
   end
 
